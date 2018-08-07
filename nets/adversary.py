46c360c36fd171775f663d37526ceb497ba05da8
Thanh-Dat Truong <ttdat@selab.hcmus.edu.vn>
2018-08-07 14:11:24 -0500

    Adjust Leaky ReLu

--- nets/adversary.py
+++ nets/adversary.py
@@ -1,9 +1,10 @@
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
+#import tflearn
 
-def adversarial_discriminator(net, layers = [500, 500], scope='adversary', leaky=False, weight_decay = 2.5e-5):
+def adversarial_discriminator(net, layers = [500, 500], scope='adversary', leaky=False, weight_decay = 2.5e-5, alpha = 0.1):
     if leaky:
-        activation_fn = tf.nn.leaky_relu
+        activation_fn = lambda x: tf.nn.leaky_relu(x, alpha = alpha)
     else:
         activation_fn = tf.nn.relu
 
