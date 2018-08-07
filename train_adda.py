18b1c6c066e551a9169c83b7382854474723bce9
Thanh-Dat Truong <ttdat@selab.hcmus.edu.vn>
2018-08-07 14:12:33 -0500

    Fix bug learning rate

--- train_adda.py
+++ train_adda.py
@@ -30,13 +30,15 @@ def parse_args():
     #Learning Configuration
     parser.add_argument("--model", help = "Model", type = str, default = "lenet")
     parser.add_argument("--adversary_leaky", help = "Adversary Leaky", type = bool, default = False)
-    parser.add_argument("--learning_rate", help = "Learning Rate", type = float, default = 0.0001)
+    parser.add_argument("--learning_rate_discriminator", help = "Learning Rate", type = float, default = 1e-5)
+    parser.add_argument("--learning_rate_generator", help = "Learning Rate of Genertor", type = float, default = 1e-6)
     parser.add_argument("--batch_size", help = "Batch Size", type = int, default = 16)
     parser.add_argument("--num_iters", help = "Number of Iterations", type = int, default = 30000)
     parser.add_argument("--solver", help = "Choice optimizer", type = str, default = "adam")
     parser.add_argument("--checkpoint_steps", help = "Checkpoint Step", type = int, default = 100)
     parser.add_argument("--lr_decay_steps", help = "Decay Steps of Learning Rate", type = int, default = None)
     parser.add_argument("--lr_decay_rate", help = "Decay Rate", type = float, default = 0.1)
+    parser.add_argument("--feature_name", help = "Feature Name", type = str, default = None)
     return parser.parse_args()
 
 def main():
@@ -71,26 +73,41 @@ def main():
     ##################
     model_fn = get_model_fn(options.model)
 
-    source_ft, source_layers = model_fn(source_images, scope = "source_network", is_training = True,
+    source_ft, source_layers = model_fn(source_images, scope = "source_network", is_training = False,
                                         num_classes = source_dataset.num_classes, image_size = options.image_size)
-    target_ft, targte_layers = model_fn(target_images, scope = "target_network", is_training = True,
+    target_ft, target_layers = model_fn(target_images, scope = "target_network", is_training = True,
                                         num_classes = target_dataset.num_classes, image_size = options.image_size)
+    source_pred = source_ft
+    target_pred = target_ft
+
+    if options.feature_name == "None" or options.feature_name is None:
+        feature_name = None
+    else:
+        feature_name = options.feature_name
+
+    if (feature_name is not None) and (feature_name in source_layers) and (feature_name in target_layers):
+        source_ft = source_layers[options.feature_name]
+        target_ft = target_layers[options.feature_name]
+    elif feature_name is not None:
+        raise ValueError("{} was not found in scope".format(feature_name))
+
 
     source_ft = tf.reshape(source_ft, [-1, int(source_ft.get_shape()[-1])])
     target_ft = tf.reshape(target_ft, [-1, int(target_ft.get_shape()[-1])])
+
     adversary_ft = tf.concat([source_ft, target_ft], 0)
 
     source_adversary_label = tf.zeros([tf.shape(source_ft)[0]], tf.int32)
     target_adversary_label = tf.ones([tf.shape(target_ft)[0]], tf.int32)
     adversary_label = tf.concat([source_adversary_label, target_adversary_label], 0)
 
-    adversary_logits = adversarial_discriminator(adversary_ft, leaky = options.adversary_leaky)
+    adversary_logits = adversarial_discriminator(adversary_ft, leaky = options.adversary_leaky, layers = [512, 500])
     mapping_loss = tf.losses.sparse_softmax_cross_entropy(1 - adversary_label, adversary_logits)
     adversary_loss = tf.losses.sparse_softmax_cross_entropy(adversary_label, adversary_logits)
 
     domain_accuracy = slim.metrics.accuracy(tf.argmax(adversary_logits, axis = 1), tf.cast(adversary_label, tf.int64))
-    source_accuracy = slim.metrics.accuracy(tf.argmax(source_ft, axis = 1), tf.cast(source_labels, tf.int64))
-    target_accuracy = slim.metrics.accuracy(tf.argmax(target_ft, axis = 1), tf.cast(target_labels, tf.int64))
+    source_accuracy = slim.metrics.accuracy(tf.argmax(source_pred, axis = 1), tf.cast(source_labels, tf.int64))
+    target_accuracy = slim.metrics.accuracy(tf.argmax(target_pred, axis = 1), tf.cast(target_labels, tf.int64))
 
 
     source_vars = utils.collect_vars("source_network", prepend_scope = options.model)
@@ -107,20 +124,24 @@ def main():
     target_saver = tf.train.Saver(target_vars)
     adversary_saver = tf.train.Saver(adversary_vars)
 
-    learning_rate_op  = tf.Variable(options.learning_rate, name='learning_rate', trainable=False)
-    if options.solver == 'sgd':
-        optimizer = tf.train.MomentumOptimizer(learning_rate_op, 0.99)
-    elif options.solver == "adam":
-        optimizer = tf.train.AdamOptimizer(learning_rate_op, 0.5)
+    learning_rate_generator_op  = tf.Variable(options.learning_rate_generator, name='learning_rate_generator', trainable=False)
+    learning_rate_discriminator_op  = tf.Variable(options.learning_rate_discriminator, name='learning_rate_discriminator', trainable=False)
 
-    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope = "source_network")
+    if options.solver == 'sgd':
+        generator_optimizer = tf.train.MomentumOptimizer(learning_rate_generator_op, 0.99, name = "generator_discriminator")
+        discriminator_optimizer = tf.train.MomentumOptimizer(learning_rate_discriminator_op, 0.99, name = "discriminator_optimizer")
+    elif options.solver == "adam":
+        generator_optimizer = tf.train.AdamOptimizer(learning_rate_generator_op, 0.5, name = "generator_discriminator")
+        discriminator_optimizer = tf.train.AdamOptimizer(learning_rate_discriminator_op, 0.5, name = "discriminator_optimizer")
+
+
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope = "target_network")
     if update_ops:
         with tf.control_dependencies(update_ops):
-            mapping_step = optimizer.minimize(mapping_loss, var_list=list(target_vars.values()))
+            mapping_step = generator_optimizer.minimize(mapping_loss, var_list=list(target_vars.values()))
     else:
-        mapping_step = optimizer.minimize(mapping_loss, var_list=list(target_vars.values()))
-
-    adversary_step = optimizer.minimize(adversary_loss, var_list=list(adversary_vars.values()))
+        mapping_step = generator_optimizer.minimize(mapping_loss, var_list=list(target_vars.values()))
+    adversary_step = discriminator_optimizer.minimize(adversary_loss, var_list=list(adversary_vars.values()))
 
     config = tf.ConfigProto(device_count=dict(GPU=1))
     config.gpu_options.allow_growth = True
@@ -163,14 +184,15 @@ def main():
     ## Training ##
     ##############
     timer = utils.Timer()
-    current_learning_rate = options.learning_rate
+    current_learning_rate_generator = options.learning_rate_generator
+    current_learning_rate_discriminator = options.learning_rate_discriminator
     for iter in xrange(1, options.num_iters + 1):
         timer.tic()
-        _mapping_loss, _adversary_loss, _, _, src_acc, tgt_acc, dom_acc, summary, lr  = sess.run([
+        _mapping_loss, _adversary_loss, _, _, src_acc, tgt_acc, dom_acc, summary, lr_gen, lr_dis  = sess.run([
                                                 mapping_loss, adversary_loss,
                                                 mapping_step, adversary_step,
                                                 source_accuracy, target_accuracy, domain_accuracy,
-                                                summary_op, learning_rate_op])
+                                                summary_op, learning_rate_generator_op, learning_rate_discriminator_op])
 
         summary_writer.add_summary(summary, iter)
         print("Iteration [{}/{}]:".format(iter, options.num_iters))
@@ -179,7 +201,8 @@ def main():
         print("\t>> Domain Accuracy:\t{}".format(dom_acc))
         print("\t>> Source Accuracy:\t{}".format(src_acc))
         print("\t>> Target Accuracy:\t{}".format(tgt_acc))
-        print("\t>> Learning Rate:\t{}".format(lr))
+        print("\t>> Generator Learning Rate:\t{}".format(lr_gen))
+        print("\t>> Discriminator Learning Rate:\t{}".format(lr_gen))
         print("\t>> Executed Time:\t{} sec/iter".format(timer.toc()))
 
         if (iter % options.checkpoint_steps) == 0:
@@ -189,7 +212,10 @@ def main():
 
 
         if options.lr_decay_steps is not None and (iter % options.lr_decay_steps) == 0:
-            current_learning_rate = sess.run(learning_rate_op.assign(current_learning_rate * options.lr_decay_rate))
+            current_learning_rate_generator = sess.run(learning_rate_generator_op.assign(
+                                            current_learning_rate_generator * options.lr_decay_rate))
+            current_learning_rate_discriminator = sess.run(learning_rate_discriminator_op.assign(
+                                            current_learning_rate_discriminator * options.lr_decay_rate))
 
     coord.request_stop()
     coord.join(threads)
